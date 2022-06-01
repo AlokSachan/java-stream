ad059a5555aea352e22a22282818b4a59389685a
AlokSachan <alok.sachan.83@gmail.com>
2022-06-01 15:46:48 +0530

    Stream using filter

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -1,7 +1,6 @@
 package lectures;
 
 import beans.Person;
-import com.google.common.collect.ImmutableList;
 import mockdata.MockData;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
@@ -13,43 +12,33 @@ import java.util.stream.Collectors;
 
 public class Lecture1 {
 
+
   @Test
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
+    List<Person> youngPeople= Lists.newArrayList();
+    int counter =0;
+for(Person person: people){
+  if(person.getAge() <= 18 && counter<10){
+    youngPeople.add(person);
+    counter++;
+  }
+}
 
-    List<Person> youngPeople = Lists.newArrayList();
-
-    final int limit = 10;
-    int counter = 0;
-
-    for( Person person : people) {
-      if(person.getAge() <= 18) {
-        youngPeople.add(person);
-        counter++;
-        if(counter == limit) {
-          break;
-        }
-      }
-    }
-
+int peopleCounter=1;
     for (Person young : youngPeople) {
-      System.out.println(young);
+      System.out.println(peopleCounter + " : person  ->  "+young);
+      peopleCounter++;
     }
-
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
-    List<Person> youngPeople = people.stream()
-        .filter(person -> person.getAge() <= 18)
-        .limit(10)
-        .collect(Collectors.toList());
-
-    youngPeople.forEach(System.out::println);
-
+    MockData.getPeople()
+            .stream()
+            .filter(person -> person.getAge() <=18)
+            .limit(10)
+            .collect(Collectors.toList())
+            .forEach(person-> System.out.println(person));
   }
 }
