7d6b57b6b8a56c4c19ee755a66287432ba0fe7e3
AlokSachan <alok.sachan.83@gmail.com>
2022-06-01 16:29:14 +0530

    using IntStream

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -5,46 +5,48 @@ import mockdata.MockData;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lecture2 {
 
   @Test
   public void range() throws Exception {
-
     System.out.println("for i");
-
     for (int i = 0; i <= 10; i++) {
       System.out.println(i);
     }
+    System.out.println("Using IntStream Inclusive");
+    IntStream.range(0, 10)
+            .boxed()
+            .collect(Collectors.toList())
+            .forEach(System.out::print);
 
-    System.out.println("exclusive");
-
-    IntStream.range(0, 10).forEach(System.out::println);
-
-    System.out.println("inclusive");
-
-    IntStream.rangeClosed(0, 10).forEach(System.out::println);
-
+    System.out.println("Using IntStream Exclusive");
+    IntStream.rangeClosed(0, 10)
+            .boxed()
+            .collect(Collectors.toList())
+            .forEach(System.out::print);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
+
     List<Person> people = MockData.getPeople();
-
     IntStream.range(0, people.size())
-        .forEach(index -> {
-          Person person = people.get(index);
-          System.out.println(person);
-        });
-
+        .forEach(
+            index -> {
+              Person person = people.get(index);
+              System.out.println(person);
+            });
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+
+    IntStream.iterate(0, module -> module + 1)
+            .filter(input-> input %2 ==0)
+            .limit(10)
+            .forEach(System.out::println);
   }
 }
