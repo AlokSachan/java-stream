554146dcf0f713e46477fd487078e019f24e3634
AlokSachan <alok.sachan.83@gmail.com>
2022-06-01 16:50:15 +0530

    Finding Min/Max using stream

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,10 +1,10 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Comparator;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,26 +12,18 @@ public class Lecture3 {
 
   @Test
   public void min() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
-    Integer min = numbers.stream()
-        .min(Comparator.naturalOrder())
-        .get();
-
+    Integer min = ImmutableList.of(10, 2, 3, 100, 23, 93, 99, 1, 20)
+            .stream()
+            .min(Comparator.naturalOrder())
+            .get();
     assertThat(min).isEqualTo(1);
-    System.out.println(min);
-
   }
 
   @Test
   public void max() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
-    Integer max = numbers.stream()
-        .max(Comparator.naturalOrder())
-        .get();
-
+    Integer max= ImmutableList.of(1, 2, 3, 100, 23, 93, 99)
+            .stream().max(Comparator.naturalOrder())
+            .get();
     assertThat(max).isEqualTo(100);
-    System.out.println(max);
   }
 }
